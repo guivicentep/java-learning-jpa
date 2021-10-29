fe71d2a896093af7b41aae9ff5a78fcf8d59e051
Guilherme <guivicente.gvp@hotmail.com>
2021-10-29 09:25:15 -0300

    First commit

---
+++ src/main/java/br/com/gui/loja/modelo/Categoria.java
@@ -0,0 +1,33 @@
+package br.com.gui.loja.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	
+	public Categoria() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Categoria(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
---
+++ src/main/java/br/com/gui/loja/testes/CadastroDeProduto.java
@@ -0,0 +1,49 @@
+package br.com.gui.loja.testes;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import br.com.gui.loja.dao.CategoriaDAO;
+import br.com.gui.loja.dao.ProdutoDAO;
+import br.com.gui.loja.modelo.Categoria;
+import br.com.gui.loja.modelo.Produto;
+import br.com.gui.loja.util.JPAUtil;
+
+public class CadastroDeProduto {
+	
+	public static void main(String[] args) {
+		cadastrarProduto();
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDAO produtoDAO = new ProdutoDAO(em);
+		
+		Produto p = produtoDAO.buscarPorId(1l);
+		System.out.println(p.getPreco());
+		
+		List<Produto> todos = produtoDAO.buscarPorNomeDaCategoria("CELULARES");
+		todos.forEach(p2 -> System.out.println(p.getNome()));
+		
+		BigDecimal precoDoProduto = produtoDAO.buscarPrecoDoProdutoComNome("Xiaomi Redmi");
+		System.out.println("AAAAAAAAAAA" + precoDoProduto);
+	}
+
+	private static void cadastrarProduto() {
+		Categoria celulares = new Categoria("CELULARES");
+		
+		Produto celular = new Produto("Xiaomi Redmi", "Nice", new BigDecimal("800"), celulares );
+
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDAO produtoDAO = new ProdutoDAO(em);
+		CategoriaDAO categoriaDAO = new CategoriaDAO(em);
+		
+		em.getTransaction().begin();
+		
+		categoriaDAO.cadastrar(celulares);
+		produtoDAO.cadastrar(celular);
+		
+		
+		em.getTransaction().commit();
+		em.close();
+	}
+}
